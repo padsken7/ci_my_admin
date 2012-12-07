c6fcf9989541c5401c4f84f7e4faa1b828d24c06
padsken7 <padsken7@gmail.com>
2012-12-07 13:30:11 +0200

    Update application/config/admin.php

--- application/config/admin.php
+++ application/config/admin.php
@@ -34,7 +34,7 @@
 |		// массив id полей, для которых применяем текстовый редактор
 |		$config['nicedit_for'] = array('text');
 |
-*/	
+*/
 	
 		// таблицы с которыми будем работать
 		$tables['sub_cats']['add'] = 'Добавить подкатегорию';
@@ -51,40 +51,6 @@
 		$comments['sub_cats']['name'] = 'на латинице (отображается в адресной строке) напр.: motoroils';
 		$comments['sub_cats']['cid'] = 'число 1 (для легкового транспорта), 2 (для коммерческого транспорта) или 3 (для оборудования и спецтехники)';
 
-
-/*
-
-Rule				Parameter		Description
-
-required	        No	    		Returns FALSE if the form element is empty.
-matches	            Yes	    		Returns FALSE if the form element does not match the one in the parameter.	matches[form_item]
-is_unique	        Yes	    		Returns FALSE if the form element is not unique to the table and field name in the parameter.	is_unique[table.field]
-min_length	        Yes	    		Returns FALSE if the form element is shorter then the parameter value.	min_length[6]
-max_length	        Yes	    		Returns FALSE if the form element is longer then the parameter value.	max_length[12]
-exact_length	    Yes	    		Returns FALSE if the form element is not exactly the parameter value.	exact_length[8]
-greater_than	    Yes	    		Returns FALSE if the form element is less than the parameter value or not numeric.	greater_than[8]
-less_than	        Yes	    		Returns FALSE if the form element is greater than the parameter value or not numeric.	less_than[8]
-alpha	            No	    		Returns FALSE if the form element contains anything other than alphabetical characters.	 
-alpha_numeric	    No	    		Returns FALSE if the form element contains anything other than alpha-numeric characters.	 
-alpha_dash	        No	    		Returns FALSE if the form element contains anything other than alpha-numeric characters, underscores or dashes.	 
-numeric	            No	    		Returns FALSE if the form element contains anything other than numeric characters.	 
-integer	            No	    		Returns FALSE if the form element contains anything other than an integer.	 
-decimal	            Yes	    		Returns FALSE if the form element is not exactly the parameter value.	 
-is_natural	        No	    		Returns FALSE if the form element contains anything other than a natural number: 0, 1, 2, 3, etc.	 
-is_natural_no_zero	No	    		Returns FALSE if the form element contains anything other than a natural number, but not zero: 1, 2, 3, etc.	 
-valid_email	        No	    		Returns FALSE if the form element does not contain a valid email address.	 
-valid_emails	    No	    		Returns FALSE if any value provided in a comma separated list is not a valid email.	 
-valid_ip	        No	    		Returns FALSE if the supplied IP is not valid. Accepts an optional parameter of "IPv4" or "IPv6" to specify an IP format.	 
-valid_base64	    No	    		Returns FALSE if the supplied string contains anything other than valid Base64 characters.
-
-xss_clean	    	No	            Runs the data through the XSS filtering function, described in the Input Class page.
-prep_for_form	    No	            Converts special characters so that HTML data can be shown in a form field without breaking it.
-prep_url	        No	            Adds "http://" to URLs if missing.
-strip_image_tags	No	            Strips the HTML from image tags leaving the raw URL.
-encode_php_tags	    No	            Converts PHP tags to entities.
-
-*/
-		
 		// валидация
 		$validation['products']['title'] = 'required|min_length[3]|xss_clean|htmlspecialchars';
 		$validation['products']['text'] = 'required|xss_clean';
@@ -108,14 +74,13 @@ encode_php_tags	    No	            Converts PHP tags to entities.
 | -------------------------------------------------------------------------
 | CONFIG LOADING
 | -------------------------------------------------------------------------
-*/		
+*/	
 		
 		// загрузка опций
 		$config['tables'] = $tables;
 		$config['comments'] = $comments;
 		$config['validation'] = $validation;
 		$config['relations'] = $relations;
-		
 		
 
 		
